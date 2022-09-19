9ca9b4c7956eb239d21cbb9479ce4a1def0b7f7a
derrydwi <derrydwiaditya@gmail.com>
2022-09-19 21:24:05 +0700

    refactor: optimize production

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const cors = require('cors');
 
 require('dotenv').config();
 
-//Connect to database
 try {
   mongoose.connect(process.env.MONGODB_URL, {
     useUnifiedTopology: true,
@@ -25,9 +24,7 @@ const app = express();
 app.use(compression());
 app.use(cors());
 
-// parse requests of content-type - application/json
 app.use(express.json());
-// parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/data', dataRoutes);
