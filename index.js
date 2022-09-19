79d40008dc9a68bda2b268628d35a6c0d0006fe3
derrydwi <derrydwiaditya@gmail.com>
2022-09-19 20:13:12 +0700

    feat: optimize for production

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require('express');
-const app = express();
 const mongoose = require('mongoose');
+const compression = require('compression');
 const dataRoutes = require('./routes/data');
-const reportRoutes = require('./routes/report');
 const cors = require('cors');
 
 require('dotenv').config();
@@ -21,6 +20,9 @@ process.on('unhandledRejection', (error) => {
   console.log('unhandledRejection', error.message);
 });
 
+const app = express();
+
+app.use(compression());
 app.use(cors());
 
 // parse requests of content-type - application/json
@@ -29,8 +31,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/data', dataRoutes);
-app.use('/report', reportRoutes);
 
-app.listen(process.env.PORT || 8080, function () {
-  console.log('App running!');
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
 });
