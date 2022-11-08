faa04a1a83f620d3ac66506cf08c39c236cd2c48
derrydwi <derrydwiaditya@gmail.com>
2022-11-08 15:22:47 +0700

    refactor: update user role models

--- models/user.js
+++ models/user.js
@@ -9,7 +9,7 @@ const userSchema = new mongoose.Schema(
     picture: String,
     role: {
       type: String,
-      enum: ['student', 'teacher'],
+      enum: ['student', 'teacher', 'superadmin'],
       default: 'student',
     },
     meetings: [
