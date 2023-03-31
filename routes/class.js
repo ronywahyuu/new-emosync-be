f2becd160c2cae477e666c42a017be8aa12909eb
Irfan Nurghiffari Muhajir <49281347+irfan44@users.noreply.github.com>
2023-03-31 02:46:25 +0000

    fix: add missing auth from class route

--- routes/class.js
+++ routes/class.js
@@ -1,7 +1,8 @@
 const router = require('express').Router()
+const auth = require('../middleware/auth')
 const _class = require('../controllers/class')
 
-router.get('/', _class.get)
-router.post('/', _class.create)
+router.get('/', auth, _class.get)
+router.post('/', auth, _class.create)
 
 module.exports = router
