dda32d430b050fca8da59e710428ff1c8575df02
Irfan Nurghiffari Muhajir <49281347+irfan44@users.noreply.github.com>
2023-03-31 08:07:11 +0000

    feat: add update & remove class routes

--- controllers/class.js
+++ controllers/class.js
@@ -18,20 +18,48 @@ const get = async (req, res, next) => {
 }
 
 const create = async (req, res, next) => {
-    try {
-      const { 'https://customclaim.com/id': createdBy } = req.auth.payload
-      const data = await _class.create({ body: req.body, createdBy })
-      if (!data) {
-        return res.status(404).send({ message: "Data can't be saved!" })
-      }
-      return res.status(201).send({ data })
-    } catch (error) {
-      console.log(error)
-      next(error)
+  try {
+    const { 'https://customclaim.com/id': createdBy } = req.auth.payload
+    const data = await _class.create({ body: req.body, createdBy })
+    if (!data) {
+      return res.status(404).send({ message: "Data can't be saved!" })
     }
+    return res.status(201).send({ data })
+  } catch (error) {
+    console.log(error)
+    next(error)
   }
+}
+
+const update = async (req, res, next) => {
+  try {
+    const data = await _class.update({ id: req.params.id, body: req.body })
+    if (!data) {
+      return res.status(404).send({ message: 'Data not found!' })
+    }
+    return res.status(200).send({ data })
+  } catch (error) {
+    console.log(error)
+    next(error)
+  }
+}
+
+const remove = async (req, res, next) => {
+  try {
+    const data = await _class.remove({ id: req.params.id })
+    if (!data) {
+      return res.status(404).send({ message: 'Data not found!' })
+    }
+    return res.status(200).send({ data })
+  } catch (error) {
+    console.log(error)
+    next(error)
+  }
+}
 
 module.exports = {
   get,
   create,
+  update,
+  remove,
 }
--- routes/class.js
+++ routes/class.js
@@ -4,5 +4,7 @@ const _class = require('../controllers/class')
 
 router.get('/', auth, _class.get)
 router.post('/', auth, _class.create)
+router.patch('/:id', auth, _class.update)
+router.delete('/:id', auth, _class.remove)
 
 module.exports = router
