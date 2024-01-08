90133360dfac85870666345e1e0f9322464163f3
NikaQisty <nikaqistyy@upi.edu>
2024-01-09 01:12:25 +0700

    add intervention words

--- utils/interventionWords.js
+++ utils/interventionWords.js
@@ -38,10 +38,76 @@ const ANGRY = {
     'Jadikanlah ledakan amarahmu, seperti ledakan petir yang menyinari kegelapan! ⚡⛈️',
     'Memang tidak mudah untuk mengontrol amarah, tapi kamu pasti bisa mengatasinya! 😉',
     'Terima kasih karena sudah bertahan, tetap semangat! 😃'
+  ], 
+  negative: [
+    'Jangan biarkan kemarahan ini merusak fokusmu yaa 💪',
+    'Tetap fokus!! Masih banyak materi yang harus dipahami😅',
+    'Semua pasti akan berlalu, berlalu-lalang. Sabar yaa 😅',
+    'Jangan marah dongg selow 😣',
+    'Kamu kerenn, tapi jangan marah-marah dong jadi ga keren 😓',
+    'Daripada marah-marah, mendingan fokus lagi yuk! 😃',
+    'Sabar-sabar, orang sabar pahalanya besar 😅',
+    'Ubah amarahmu menjadi kekuatan untuk mencapai tujuan! 💪😠',
+    'Kamu memegang kendali dirimu jangan sampai amarah yang mengendalikanmu! 😉',
+    'Dua tiga ikan hiu, loh kamu jangan begitu 😣'
+  ]
+}
+
+const FEAR = {
+  positive: [
+    'Kamu akan baik-baik aja kok 😊',
+    'Tetap semangat!! Setiap langkah adalah tantangan untuk tumbuh dan berkembang 🌞',
+    'Apa yang kamu rasakan saat ini adalah sebuah tantangan, jangan putus asa yaa 😊',
+    'Tidak masalah jika kamu merasa tidak nyaman, setidaknya kamu sudah berusaha 🤩',
+    'Kamu lebih hebat dari yang kamu pikirkan, tetap bertahan yaa 🤗',
+    'Kamu keren jangan cepet nyerah ya! 😎',
+    'Proses belajar memang penuh tantangan, kamu pasti bisa bertahan! 🤗',
+    'Memang tidak mudah untuk merasa baik-baik saja, tapi kamu pasti bisa mengatasinya! 😉',
+    'Kamu pasti bisa melawan rasa itu, ayoo semangat! 🐱',
+    'Makan indomi sambil nonton Anya, kamu pasti bisa melewatinya! 🤩'
+  ],
+  negative: [
+    'Ayo bangkit, jangan takut hidup memang penuh tantangan! 🙃',
+    'Tetap fokus! Selalu ingat tujuanmu 😉',
+    'Jangan takut, takutlah hanya saat kamu tidak pernah mencoba 😃',
+    'Tetap tenang dan fokus yaa semua akan baik-baik aja 😉',
+    'Ayo semangat katanya mau jadi hokage, jangan takut!! 😃',
+    'Ayo hadapi kamu kan pemberani 😎',
+    'Lawan rasa takut itu, selalu ingat tujuan utamamu di awal! 💪',
+    'Kamu harus beranikan diri untuk mencoba dan melewatinya! 🤩',
+    'Jadikanlah rasa takut untuk mendorongmu menjadi lebih tangguh 💪😏',
+    'Si Heru mancing ikan nila, kamu harus jangan putus asa 🎣😎'
+  ]
+}
+
+const DISGUST = {
+  positive: [
+    'Semakin kuat kamu bertahan di zona yang tidak nyaman, maka kamu semakin hebat! 😎',
+    'Pikirkan hal-hal yang membuatmu bahagia, kamu pasti bisa! 🐱',
+    'Ayoo kamu pasti bisa menahan rasa tidak nyaman ini hingga akhir😣',
+    'Semangatt, anggap aja perasaan ini seperti sebuah tantangan yang harus kamu taklukan! 🤗',
+    'Jadikan rasa tidak nyaman ini sebagai motivasi untuk lebih tangguh! 😉',
+    'Ketekunan adalah kunci keberhasilan, maka ayo tamatkan semua ini 😵',
+    'Tidak masalah, setidaknya kamu sudah mencoba 🤗',
+    'Emang sih perasaan ini tidak enak, tapi kamu pasti bisa bertahan! 💪',
+    'Kamu pasti bisa melewati situasi ini, jangan menyerah ya! 😃',
+    'Buah yang lagi sakit itu alpucat, cepat buktikan kalau kamu hebat 🥑😎'    
+  ],
+  negative: [
+    'Tetap hadapi perasaan itu dan kamu akan mengerti makna di dalamnya! 😇',
+    'Tetap fokus dan tahan rasa itu maka kamu akan paham betapa tangguhnya dirimu! 💪',
+    'Perasaan ini hanya sesaat, tetap hadapi untuk mendapatkan ilmu yang abadi 📖',
+    'Asal kamu mau berusaha, kamu pasti bisa melewatinya 😎',
+    'Banyak yang lebih parah dari kamu, masa kamu kalah! 😃',
+    'Jangan sampai rasa tidak nyaman ini menghambatmu! 😅',
+    'Hanya orang hebat yang bisa melewati ini, kamu juga harus bisa! 😅',
+    'Zona nyaman itu membosankan, kamu harus hadapi tantangan! 😬',
+    'Tidak boleh ada kata menyerah dalam kamusmu! 🤩',
+    'Si dadang beli kopeah, kamu sabar dulu yah 😅'
   ]
 }
 
 
 module.exports = {
-  SAD
+  SAD, ANGRY, FEAR, DISGUST
 }
